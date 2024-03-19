3f1732ae72ea31272e22706385a6e8206c2379a3
KingTingTheGreat <jting@bu.edu>
2024-03-19 16:41:45 -0400

    terminal settings updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,28 +1,19 @@
-path = 'C:/Users/jting/OneDrive/Pictures/Wallpapers'
-
-function randImg()
-	local path = 'C:/Users/jting/OneDrive/Pictures/Wallpapers/'
-	local files = {}
-	for file in io.popen('ls "'..path..'"'):lines() do
-		table.insert(files, file)
-	end
-	if #files == 0 then
-		return nil
-	end
-	local out = path .. files[math.random(1, #files)]
-	local file = io.open("output.txt", "w")
-	if file then
-		file:write(out)
-		file:close()
-	end
-end
+--local function randImg()
+--	local path = 'C:/Users/jting/OneDrive/Pictures/Wallpapers/'
+--	local files = {'boygenius.jpg', 'boygenius-suits.png', 'boygenius1.png', 'boygenius2.png', 'boygenius3.png'}
+--	if #files == 0 then
+--		return nil
+--	end
+--	local image = files[math.random(1, #files)]
+--	return path .. image
+--end
 
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
 --config.default_domain = 'WSL:Ubuntu'
-config.font = wezterm.font 'BlexMono Nerd Font Mono Text'
+config.font = wezterm.font_with_fallback {'BlexMono Nerd Font Mono Text'}
 config.color_scheme = 'Thayer Bright'
 --config.color_scheme = 'tokyonight_night'
 
@@ -32,9 +23,10 @@ config.initial_cols = 100
 
 config.default_prog = { "C:/Program Files/Git/bin/bash.exe" }
 
-config.window_background_image = path .. '/boygenius-suits.png'
+--config.window_background_image = path .. '/boygenius-suits.png'
 --config.window_background_image = randImg()
 --config.window_background_image = os.excecute('./img.sh') .. '/boygenius-suits.png'
+config.window_background_image = 'C:/Users/jting/OneDrive/Pictures/Wallpapers/boygenius.jpg'
 
 config.window_background_image_hsb = {
 	brightness = 0.2,
