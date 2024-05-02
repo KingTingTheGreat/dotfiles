85f2b136745161fa0f718ec4b1199315264792f7
Jeffrey Ting <jting@bu.edu>
2024-05-02 17:16:59 -0400

    give up on random background image. this is a nice setup rn

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,3 @@
--- local function trytwo()
--- 	local path = 'C:/Users/jting/OneDrive/Pictures/Wallpapers/'
--- 	local files = {}
--- 	for dir in io.popen([[dir "C:/Users/jting/OneDrive/Pictures/Wallpapers"]]):lines() do
--- 		table.insert(files, dir)
--- 	end
--- 	print(path .. files[math.random(1, #files)])
--- end
--- 
--- trytwo()
 local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
@@ -22,10 +12,6 @@ config.initial_cols = 100
 
 config.default_prog = { "C:/Program Files/Git/bin/bash.exe" }
 
---config.window_background_image = path .. '/boygenius-suits.png'
---config.window_background_image = randImg()
---config.window_background_image = os.excecute('./img.sh') .. '/boygenius-suits.png'
--- config.window_background_image = 'C:/Users/jting/OneDrive/Pictures/Wallpapers/boygenius.jpg'
 config.window_background_image = "C:/Users/jting/OneDrive/Pictures/Wallpapers/the-rest.png"
 
 config.window_background_image_hsb = {
