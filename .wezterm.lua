1d8df084edb6321b2334d10767ad3ccaea9d9fef
Jeffrey Ting <jting@bu.edu>
2024-08-24 20:29:58 -0400

    formating

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,14 +2,14 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback {'BlexMono Nerd Font Mono Text'}
+config.font = wezterm.font_with_fallback { 'BlexMono Nerd Font Mono Text' }
 -- config.color_scheme = 'Thayer Bright'
 config.color_scheme = 'Aardvark Blue'
 
 config.window_decorations = "RESIZE"
 
 config.initial_cols = 120
-config.initial_rows= 30
+config.initial_rows = 30
 
 config.default_prog = { "C:/Program Files/Git/bin/bash.exe" }
 
